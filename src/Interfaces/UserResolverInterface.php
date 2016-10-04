1c6a69e8ad462ae8b6b70ff2a47f4165f6df0a6b
Arjon Jason Castro <ajcastro29@gmail.com>
2016-10-04 15:56:32 +0800

    Add docblock

--- src/Interfaces/UserResolverInterface.php
+++ src/Interfaces/UserResolverInterface.php
@@ -2,7 +2,16 @@
 
 namespace SedpMis\Transactions\Interfaces;
 
+/**
+ * Resolve the user from a specific signatory.
+ */
 interface UserResolverInterface
 {
+    /**
+     * Return the user of a specific signatory.
+     *
+     * @param  \SedpMis\Transactions\Models\Interfaces\SignatoryInterface $signatory
+     * @return \User
+     */
     public function getUser($signatory);
 }
