283e082927623fe26653d9cc3469fa37a50c59dd
Arjon Jason Castro <ajcastro29@gmail.com>
2016-10-06 19:57:22 +0800

    Add interfaces methods.

--- src/Models/Interfaces/SignatoryInterface.php
+++ src/Models/Interfaces/SignatoryInterface.php
@@ -4,4 +4,17 @@ namespace SedpMis\Transactions\Models\Interfaces;
 
 interface SignatoryInterface
 {
+    /**
+     * SignatorySet relation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function signatorySet();
+
+    /**
+     * SignatoryAction relation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function signatoryAction();
 }
--- src/Models/Interfaces/TransactionInterface.php
+++ src/Models/Interfaces/TransactionInterface.php
@@ -4,4 +4,45 @@ namespace SedpMis\Transactions\Models\Interfaces;
 
 interface TransactionInterface
 {
+    /**
+     * Last transaction approval relation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function lastTransactionApproval();
+
+    /**
+     * Documents relation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function documents();
+
+    /**
+     * Menu relation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function menu();
+
+    /**
+     * Transaction approvals relation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function transactionApprovals();
+
+    /**
+     * Current user signatory relation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function curUserSignatory();
+
+    /**
+     * Current signatory relation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function curSignatory();
 }
