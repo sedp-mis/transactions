6756992c7195316e802f4da7cd2aa798295c5c60
Arjon Jason Castro <ajcastro29@gmail.com>
2016-10-07 15:25:08 +0800

    Fix

--- src/Repositories/Transaction/TransactionRepositoryEloquent.php
+++ src/Repositories/Transaction/TransactionRepositoryEloquent.php
@@ -400,9 +400,6 @@ class TransactionRepositoryEloquent extends BaseBranchRepositoryEloquent impleme
     public function getHistoricalTransactions($userId)
     {
         $query = $this->prepareQuery();
-
-        $query->where();
-
         $query->whereIn('transactions.id', $this->transactionApproval->where('user_id', $userId)->lists('transaction_id'));
         $query->where('transactions.current_user_signatory', '!=', $userId);
 
