483a7332008288780c0f142aa09158996d03f88b
Arjon Jason Castro <ajcastro29@gmail.com>
2016-10-07 15:25:51 +0800

    Fix

--- src/Repositories/Transaction/TransactionRepositoryEloquent.php
+++ src/Repositories/Transaction/TransactionRepositoryEloquent.php
@@ -384,7 +384,6 @@ class TransactionRepositoryEloquent extends BaseBranchRepositoryEloquent impleme
     public function getTrackedTransactions($userId)
     {
         $query = $this->prepareQuery();
-
         $query->where('transactions.status', 'Q');
         $query->whereIn('transactions.id', $this->transactionApproval->where('user_id', $userId)->lists('transaction_id') ?: [null]);
 
