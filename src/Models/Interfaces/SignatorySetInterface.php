6614e6aa1c8ffaf145f8f5cd851374453395772e
Arjon Jason Castro <ajcastro29@gmail.com>
2016-10-10 11:56:10 +0800

    Add signatories() method in signatory set interface

--- src/Models/Interfaces/SignatorySetInterface.php
+++ src/Models/Interfaces/SignatorySetInterface.php
@@ -4,4 +4,10 @@ namespace SedpMis\Transactions\Models\Interfaces;
 
 interface SignatorySetInterface
 {
+    /**
+     * Signatories relation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function signatories();
 }
