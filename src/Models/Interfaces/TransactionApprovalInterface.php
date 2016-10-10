2c6de58179b289c960fc45e78e74d19b5b48cfd5
Arjon Jason Castro <ajcastro29@gmail.com>
2016-10-10 09:48:46 +0800

    Add methods in transaction approval interface

--- src/Models/Interfaces/TransactionApprovalInterface.php
+++ src/Models/Interfaces/TransactionApprovalInterface.php
@@ -4,4 +4,8 @@ namespace SedpMis\Transactions\Models\Interfaces;
 
 interface TransactionApprovalInterface
 {
+    public function signatoryAction();
+    public function signatory();
+    public function user();
+    public function job();
 }
