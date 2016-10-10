f7f3310a84f703ba81ef6573d838e868203eb471
Arjon Jason Castro <ajcastro29@gmail.com>
2016-10-10 11:58:34 +0800

    Add model method interfaces.

--- src/Models/Interfaces/TransactionApprovalInterface.php
+++ src/Models/Interfaces/TransactionApprovalInterface.php
@@ -4,8 +4,31 @@ namespace SedpMis\Transactions\Models\Interfaces;
 
 interface TransactionApprovalInterface
 {
+    /**
+     * SignatoryAction relation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function signatoryAction();
+
+    /**
+     * Signatory relation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function signatory();
+
+    /**
+     * User relation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user();
+
+    /**
+     * Job relation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function job();
 }
--- src/Models/Interfaces/UserInterface.php
+++ src/Models/Interfaces/UserInterface.php
@@ -4,4 +4,10 @@ namespace SedpMis\Transactions\Models\Interfaces;
 
 interface UserInterface
 {
+    /**
+     * Job relation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function job();
 }
