d413f458bd43af9a788638db5a3026e9b2845169
Arjon Jason Castro <ajcastro29@gmail.com>
2016-10-18 14:46:59 +0800

    Add post route for transaction action.

--- src/Router.php
+++ src/Router.php
@@ -13,13 +13,17 @@ class Router
      */
     public static function route()
     {
-        Route::get('transactions', '\SedpMis\Transactions\Controllers\TransactionsController@index');
-        Route::get('transactions/pending', '\SedpMis\Transactions\Controllers\TransactionsController@pending');
-        Route::get('transactions/approved', '\SedpMis\Transactions\Controllers\TransactionsController@approved');
-        Route::get('transactions/tracker', '\SedpMis\Transactions\Controllers\TransactionsController@tracker');
-        Route::get('transactions/history', '\SedpMis\Transactions\Controllers\TransactionsController@history');
-        Route::get('transactions/{transaction}', '\SedpMis\Transactions\Controllers\TransactionsController@show');
+        Route::get('transactions', 'SedpMis\Transactions\Controllers\TransactionsController@index');
+
+        Route::get('transactions/pending', 'SedpMis\Transactions\Controllers\TransactionsController@pending');
+        Route::get('transactions/approved', 'SedpMis\Transactions\Controllers\TransactionsController@approved');
+        Route::get('transactions/tracker', 'SedpMis\Transactions\Controllers\TransactionsController@tracker');
+        Route::get('transactions/history', 'SedpMis\Transactions\Controllers\TransactionsController@history');
+
+        Route::get('transactions/{transaction}', 'SedpMis\Transactions\Controllers\TransactionsController@show');
         Route::get('transactions/{transaction}/signatories', 'SedpMis\Transactions\Controllers\TransactionSignatoriesController@index');
         Route::get('transactions/{transaction}/document_list', 'SedpMis\Transactions\Controllers\TransactionDocumentsController@documentList');
+
+        Route::post('transactions/{transaction}/action/{action}', 'SedpMis\Transactions\Controllers\TransactionActionController@action');
     }
 }
