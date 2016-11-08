645ba532a393c1e582e7989592449a67b747e7a9
Arjon Jason Castro <ajcastro29@gmail.com>
2016-11-08 09:11:04 +0800

    Revert "Revert "Add interface methods for DocumentApprovalInterface""
    This reverts commit 8f15ab294558486522342992e09d63641e766ad4.

--- src/Models/Interfaces/DocumentApprovalInterface.php
+++ src/Models/Interfaces/DocumentApprovalInterface.php
@@ -4,4 +4,24 @@ namespace SedpMis\Transactions\Models\Interfaces;
 
 interface DocumentApprovalInterface
 {
+    /**
+     * SignatoryAction relation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function signatoryAction();
+
+    /**
+     * User relation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user();
+
+    /**
+     * Job relation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function job();
 }
