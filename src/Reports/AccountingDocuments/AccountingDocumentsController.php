9b50b02c3198b287a0c6e80f1f7090bd16b8b652
Arjon Jason Castro <ajcastro29@gmail.com>
2016-11-08 09:10:53 +0800

    Revert "Remove reports, separated into another package"
    This reverts commit e1f39c21ab306bee7787d807a038b621978a7ff9.

---
+++ src/Reports/AccountingDocuments/AccountingDocumentsController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace SedpMis\Transactions\Reports\AccountingDocuments;
+
+use SedpMis\Transactions\Reports\BaseDocument\BaseDocumentController;
+use SedpMis\Transactions\Models\DocumentType;
+use Reportings\AccountingDocument\PdfFactory;
+
+class AccountingDocumentsController extends BaseDocumentController
+{
+    /**
+     * Document type codes for this.
+     *
+     * @var array
+     */
+    protected $documentTypeIds = [DocumentType::JV, DocumentType::CD, DocumentType::OR_];
+
+    protected $eagerLoadWithTransaction = [
+        'documents.journalHead.journalEntries.account',
+    ];
+
+    public function loadReportPdf($transaction)
+    {
+        $pdf = (new PdfFactory)->make($transaction->documents->first()->documentType->id);
+
+        $dataFormat = new DataFormatter($transaction);
+
+        // Set pdf report data rows
+        $pdf->setReportData($dataFormat->reportData());
+
+        // Set pdf head details
+        if ($transaction->documents->count() == 1) {
+            $pdf->setOneHeaderTemplate(head($dataFormat->headDetails()));
+        } else {
+            $pdf->setHeaderTemplates($dataFormat->headDetails());
+        }
+
+        // // Set pdf signatories data
+        $pdf->setSignsData($dataFormat->signsData());
+
+        $pdf->showReport();
+    }
+}
