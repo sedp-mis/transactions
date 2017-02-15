828aa117ec9f8679498438c4dd04745665e09233
Arjon Jason Castro <ajcastro29@gmail.com>
2017-02-15 11:28:20 +0800

    Add method findSignatoriesByMenu()

--- src/Repositories/DocumentTypeSignatory/DocumentTypeSignatoryRepositoryEloquent.php
+++ src/Repositories/DocumentTypeSignatory/DocumentTypeSignatoryRepositoryEloquent.php
@@ -2,17 +2,36 @@
 
 namespace SedpMis\Transactions\Repositories\DocumentTypeSignatory;
 
-use SedpMis\Transactions\Models\Signatory;
+use DB;
+use App;
 
 class DocumentTypeSignatoryRepositoryEloquent implements DocumentTypeSignatoryRepositoryInterface
 {
+    protected $signatory;
+
+    public function __construct()
+    {
+        $this->signatory = App::make('\SedpMis\Transactions\Models\Interfaces\SignatoryInterface');
+    }
+
     public function findSignatoriesByTransaction($transaction, $documentTypes, $hierarchy = 1)
     {
-        return Signatory::where('signatory_set_id', $transaction->signatory_set_id)
+        return $model->where('signatory_set_id', $transaction->signatory_set_id)
             ->orderBy('hierarchy')
             ->where('hierarchy', '>=', $hierarchy)
             ->forDocumentTypesWithMenu($documentTypes->pluck('id'), $transaction->menu_id)
             ->select('signatories.*', 'transaction_document_signatories.document_type_id')
             ->get();
+    }
+
+    public function findSignatoriesByMenu($menuId, $documentTypeIds = [])
+    {
+        $q = DB::table('transaction_document_signatories')->where('menu_id', $menuId);
+
+        if ($documentTypeIds) {
+            $q->whereIn('document_type_id', $documentTypeIds);
+        }
+
+        return $this->signatory->find($q->lists('signatory_id'));
     }
 }
--- src/Repositories/DocumentTypeSignatory/DocumentTypeSignatoryRepositoryInterface.php
+++ src/Repositories/DocumentTypeSignatory/DocumentTypeSignatoryRepositoryInterface.php
@@ -5,4 +5,5 @@ namespace SedpMis\Transactions\Repositories\DocumentTypeSignatory;
 interface DocumentTypeSignatoryRepositoryInterface
 {
     public function findSignatoriesByTransaction($transaction, $documentTypes);
+    public function findSignatoriesByMenu($menuId, $documentTypeIds = []);
 }
