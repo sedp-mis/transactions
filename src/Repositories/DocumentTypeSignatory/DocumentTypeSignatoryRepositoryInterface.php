2f1c27d6255f9555872efef0432e82e8e2b9ac2c
Arjon Jason Castro <ajcastro29@gmail.com>
2017-02-17 16:39:00 +0800

    Fix

--- src/Repositories/DocumentTypeSignatory/DocumentTypeSignatoryRepositoryInterface.php
+++ src/Repositories/DocumentTypeSignatory/DocumentTypeSignatoryRepositoryInterface.php
@@ -4,6 +4,5 @@ namespace SedpMis\Transactions\Repositories\DocumentTypeSignatory;
 
 interface DocumentTypeSignatoryRepositoryInterface
 {
-    public function findSignatoriesByTransaction($transaction, $documentTypes);
     public function findSignatoriesByMenu($menuId, $documentTypeIds = []);
 }
